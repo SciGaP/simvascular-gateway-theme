7a667263ec10bb87b9d83702115545b188caaf98
Nipurn Doshi <nipunhere@gmail.com>
2017-03-12 23:00:38 -0700

    Adding Basic theme structure for Simvascular

---
+++ partials/template.blade.php
@@ -0,0 +1,115 @@
+<div class="col-md-12 ts-top-banner">
+	<!--
+	<h1>Hero Image</h1>
+	<h2>This is a simple hero unit, a simple jumbotron-style component for calling extra 7
+	attention to featured content or information.</h2>
+	<p><a class="btn btn-primary btn-large">Learn more</a></p>
+	-->
+</div>
+
+
+<div class="col-md-12 breathing-space">
+
+	<div class="row">
+		<h3 class="text-center col-md-offset-2 col-md-8">
+			The only fully opensource software package providing a complete pipeline from medical image data segmentation to patient specific blood flow simulation and analysis.
+		</h1>
+	</div>
+	<div class="row">
+		<div class="col-md-offset-2 col-md-8">
+			<h1 >Why Supercomputing?</h1>
+			<p style="color:#cccccc;">
+				Supercomputing simplifies and boosts modeling simulation, helps to make better use of computational expertise and create manufacturing apps.<br/>
+			</p>
+			<hr/>
+		</div>
+		<!--
+		<div class="col-md-2">
+			<a href="{{ URL::to('/') }}/login"><h4>Login</h4></a><hr/>
+			<a href="{{ URL::to('/') }}/create"><h4>Register</h4></a><hr/>
+		</div>
+		-->
+		
+		<div class="col-md-offset-2 col-md-8">
+          <h2>About SimVascular</h2>
+          <!-- Description -->
+          <div class="media">
+            <a class="pull-left">
+              <img class="media-object" width="60" src="http://simvascular.github.io/img/Img_About_Coro.png" alt="...">
+            </a>
+            <div class="media-body">
+              <h4 class="media-heading">Description</h4>
+              SimVascular is an open source software suite for cardiovascular simulation, providing a complete pipeline from medical image data to 3D model construction, meshing, and blood flow simulation.  SimVacular represents the state of the art in cardiovascular simulation, including advanced tools for physiologic boundary conditions, fluid structure interaction, and an accurate and efficient finite element Navier-Stokes solver.  SimVascular integrates custom code with best-in-class open source packages to support clinical and basic science research.
+            </div>
+          </div>
+          <!-- Origins -->
+          <div class="media">
+            <a class="pull-left">
+              <img class="media-object" width="55" src="http://simvascular.github.io/img/Img_About_Femoral.png" alt="...">
+            </a>
+            <div class="media-body">
+              <h4 class="media-heading">Origins</h4>
+              SimVascular originated in the lab of Charles Taylor at Stanford University, and incorporates flow solver code from the PHASTA project (SCOREC at RPI). The SimVascular revitalization project now aims to provide the first truly open source package for cardiovascular simulation with enhanced functionality for clinical and basic science research. SimVascular is now developed and maintained by an active international group of researchers in academia and industry.
+            </div>
+          </div>
+          <!-- Purpose/Synopsis -->
+          <div class="media">
+            <a class="pull-left">
+              <img class="media-object" src="http://simvascular.github.io/img/Img_About_CHD.png" alt="...">
+            </a>
+            <div class="media-body">
+              <h4 class="media-heading">Purpose</h4>
+              To provide a platform for patient-specific cardiovascular modeling and simulation that enables advances in cardiovascular disease through basic science and clinical research.
+            </div>
+          </div>
+          <!-- Audience -->
+          <div class="media">
+            <a class="pull-left">
+              <img class="media-object" src="http://simvascular.github.io/img/Img_About_Pulm.png" alt="...">
+            </a>
+            <div class="media-body">
+              <h4 class="media-heading">Audience</h4>
+              Biomedical and clinical researchers interested in medical computing, cardiovascular hemodynamics and biomechanics, including medical image segmentation, patient specific model construction, blood flow simulations, vessel biomechanics, and finite element flow simulations.
+            </div>
+          </div>
+          <!-- Broader Uses -->
+          <div class="media">
+            <a class="pull-left">
+              <img class="media-object" src="http://simvascular.github.io/img/Img_About_Aorta.png" alt="...">
+            </a>
+
+            <div class="media-body">
+              <h4 class="media-heading">Broader Uses</h4>
+              Beyond hemodynamics simulations, SimVascular has been used in simulations of medical devices, respiratory flows, and cardiac development.  SimVascular provides a full Navier-Stokes flow solver for complex geometries that can be flexibly applied across a wide range of problems in engineering and medicine.
+              <h4 class="media-heading top-buffer-md">Funding Sources</h4>
+              The SimVascular project is funded by the NSF SSI program under Program Officers Dan Katz (ACI) and Sumanta Acharya (CBET)
+            </div>
+                            
+          </div>
+
+          <h2 class="top-buffer">What's New?</h2>
+          
+          <!-- DESCRIPTION -->
+          <div class="media">
+            <a class="pull-left">
+              <img class="media-object" src="http://simvascular.github.io/img/Img_About_Coro.png" alt="...">
+            </a>
+            <div class="media-body">
+              <h4 class="media-heading">New features in SimVascular 2.0</h4>
+              <ul>
+              <li>Replacement of all prior commercial components</li>
+              <li>Opensource meshing tools with adaption</li>
+              <li>Efficient linear solver customized for blood flow</li>
+              <li>Solid modeling Boolean operations</li>
+              <li>Multiscale modelling capabilities</li>
+              <li>Coronary boundary conditions</li>
+              <li>Improved image segmentation tools</li>
+              <li>Backflow stabilization</li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
+	</div>
+
+</div>
