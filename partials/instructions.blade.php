8313cfe343e762652bf01d60e06859aba448e220
Hongzhi Lan <hongzhi@stanford.edu>
2017-09-18 19:14:51 -0700

    modify homepage; add instructions

--- partials/instructions.blade.php
+++ partials/instructions.blade.php
@@ -1,7 +1,82 @@
 <div class="container">
   <div class="content">
     <div class="text-center">
-      <h2 class="title top-space">Instructions</h2>            
+      <h2 class="title top-space">Instructions</h2>       
+
+          <div class="media">
+            <div class="media-body">
+              <!-- <h4 class="media-heading">Description</h4> -->
+              The instructions here guide users to create new accounts, create simualtion jobs, upload input/data files, run simualtion and download simulation results. 
+            </div>
+          </div>
+
+          <!-- Creaat Account -->
+          <div class="media">
+            <div class="media-body">
+              <h4 class="media-heading">Creat Account</h4>
+              If you are a college student, or work in a research institute, please use "Sign in with CILogon". In this option, under "Select An Identity Provider", find your institute, then click "Log On" and sign in with your institute ID and password.
+              <br>
+              <br>
+              If you can't find your institute, you need to create a new account by providing username, password, emal, name. 
+              <br>
+              Attention: after signing up, you can't access the computer resource until approved.
+            </div>
+          </div>
+
+          <!-- Log in -->
+          <div class="media">
+            <div class="media-body">
+              <h4 class="media-heading">Log In</h4>
+              Based on the way you created your account, choose the corresponding way to log in.
+            </div>
+          </div>
+
+          <!-- Create Project -->
+          <div class="media">
+            <div class="media-body">
+              <h4 class="media-heading">Create/Browse Projects</h4>
+              It's not required to create a new project. By default, "Default Poject" is already created for you. In one project, you can create multiple exeperiments (i.e. simulation jobs). 
+              <br>
+              <br>
+              To browse existing projects: click "Project" -> "Browse" at the upper-left corner.
+              <br>
+              To create a new project: click "Project" -> "Create" at the upper-left corner.
+            </div>
+          </div>
+
+          <!-- Create Experiments -->
+          <div class="media">
+            <div class="media-body">
+              <h4 class="media-heading">Create/Browse Experiments (Simulation Jobs)</h4>
+              To browse existing experiments: click "Experiment" -> "Browse" at the upper-left corner.
+              <br>
+              <br>
+              To create a new experiment:
+              <br>
+              1. click "Experiment" -> "Create" at the upper-left corner.
+              <br>
+              2. Give a name and click "Continue".
+              <br>
+              3. Click the corresponding buttons "Choose File" to upload required files for simulation, such as geombc.dat.1, restart.0.1, bct.dat, solver.inp, numstart.dat. For certain complicated simulation, extra files can be uploaded as optional input files.
+              <p style="margin-left: 20px">For a small simulatio job: choose queue "shared", 1 node, 1~23 cores.</p>
+              <br>
+              4. Specify compute resource.
+              <br>
+              5. Select a queue, specify the number of nodes and cores. ATTENTION: Our computer cluster has a limited number of nodes. Each node has 24 cores. Please choose appropriate queue, node and core numbers for your simulation job.
+              <p style="margin-left: 20px">For a small simulatio job: choose queue "shared", 1 node, 1~23 cores.</p>
+              <p style="margin-left: 20px">For a medium simulatio job: choose queue "compute", 1 node, 24 cores.</p>
+              <p style="margin-left: 20px">For a large simulatio job: choose queue "compute", 2 node, 48 cores.</p>
+              6. Provide email address to receive notifications (optional).
+              <br>
+              7. Click "Save and launch" to launch the simulation job. Attention: if all the nodes are occupied by other users, your job will be in the waiting queue until the number of nodes you request become available.
+              <br>
+              8. Gateway then shows a webpage about the status of the current experiment. Once the "Experiment Status" becomes "COMPLETED", files are available in "Outputs", You can download proc-xx.czf.tar.gz which contains all simulation results.You can also click "Open" in "Storage Directory" to get a list of all input and output files and download specific files.
+              <br>
+              9. You can also browse an existing exepriment and click "Clone" to create a new experiment with the same parameters.
+            </div>
+          </div>
+
+
     </div>
   </div>  
 </div>
--- partials/template.blade.php
+++ partials/template.blade.php
@@ -10,17 +10,17 @@
 
 <div class="col-md-12 breathing-space">
 
-	<div class="row">
+<!-- 	<div class="row">
 		<h3 class="text-center col-md-offset-2 col-md-8">
 			The only fully opensource software package providing a complete pipeline from medical image data segmentation to patient specific blood flow simulation and analysis.
 		</h1>
-	</div>
+	</div> -->
 	<div class="row">
 		<div class="col-md-offset-2 col-md-8">
-			<h1 >Why Supercomputing?</h1>
-			<p style="color:#cccccc;">
+			<h1 >SimVascular Supercomputing Gateway</h1>
+<!-- 			<p style="color:#cccccc;">
 				Supercomputing simplifies and boosts modeling simulation, helps to make better use of computational expertise and create manufacturing apps.<br/>
-			</p>
+			</p> -->
 			<hr/>
 		</div>
 		<!--
@@ -31,19 +31,29 @@
 		-->
 		
 		<div class="col-md-offset-2 col-md-8">
-          <h2>About SimVascular</h2>
+          <!-- <h2>About SimVascular</h2> -->
           <!-- Description -->
           <div class="media">
             <a class="pull-left">
               <img class="media-object" width="60" src="https://simvascular.github.io/img/Img_About_Coro.png" alt="...">
             </a>
             <div class="media-body">
-              <h4 class="media-heading">Description</h4>
-              SimVascular is an open source software suite for cardiovascular simulation, providing a complete pipeline from medical image data to 3D model construction, meshing, and blood flow simulation.  SimVacular represents the state of the art in cardiovascular simulation, including advanced tools for physiologic boundary conditions, fluid structure interaction, and an accurate and efficient finite element Navier-Stokes solver.  SimVascular integrates custom code with best-in-class open source packages to support clinical and basic science research.
+              <!-- <h4 class="media-heading">Description</h4> -->
+              Welcome to SimVasucular Supercomputing Gateway! Our gateway provides access to high performance computer clusters for SimVascular users who would like to run simulation with super computing (10x~ faster than desktop). The svSolver needed for running simulation is already setup and ready to use from the computer clusters. Through the gateway, SimVascular users just simply create simualtion jobs, upload input/data files, launch simualtion, and download results once the simulation is finished. There is no hassle for users to compile or setup svSolver. Detailed instructions about how to use the gateway are provided <a href="{{ URL::to('/') }}/pages/instructions">here</a>.
+              <br>
+              <br>
+              SimVascular is an open source integrated software suite for image-based cardiovascular modeling and patient-specific simulation, providing a complete pipeline from medical image visualization/segmentation, 3D model construction, meshing, to blood flow simulation and hemodynamics analysis. It is recommended that new users to SimVascular first go through <a href="http://simvascular.org">SimVascular Documentation Website</a> to understand the workflow in SimVascular. With SimVascular, users can create input/data files for simulation and upload to the gateway.
+            </div>
+          </div>
+          <!-- Policy -->
+          <div class="media">
+            <div class="media-body">
+              <h4 class="media-heading">Usage Policy</h4>
+              SimVascular Supercomputing Gateway is mainly for educational purpose. Only users from American institutions can be granted access after approval. 
             </div>
           </div>
           <!-- Origins -->
-          <div class="media">
+<!--           <div class="media">
             <a class="pull-left">
               <img class="media-object" width="55" src="https://simvascular.github.io/img/Img_About_Femoral.png" alt="...">
             </a>
@@ -51,9 +61,9 @@
               <h4 class="media-heading">Origins</h4>
               SimVascular originated in the lab of Charles Taylor at Stanford University, and incorporates flow solver code from the PHASTA project (SCOREC at RPI). The SimVascular revitalization project now aims to provide the first truly open source package for cardiovascular simulation with enhanced functionality for clinical and basic science research. SimVascular is now developed and maintained by an active international group of researchers in academia and industry.
             </div>
-          </div>
+          </div> -->
           <!-- Purpose/Synopsis -->
-          <div class="media">
+<!--           <div class="media">
             <a class="pull-left">
               <img class="media-object" src="https://simvascular.github.io/img/Img_About_CHD.png" alt="...">
             </a>
@@ -61,9 +71,9 @@
               <h4 class="media-heading">Purpose</h4>
               To provide a platform for patient-specific cardiovascular modeling and simulation that enables advances in cardiovascular disease through basic science and clinical research.
             </div>
-          </div>
+          </div> -->
           <!-- Audience -->
-          <div class="media">
+<!--           <div class="media">
             <a class="pull-left">
               <img class="media-object" src="https://simvascular.github.io/img/Img_About_Pulm.png" alt="...">
             </a>
@@ -71,9 +81,9 @@
               <h4 class="media-heading">Audience</h4>
               Biomedical and clinical researchers interested in medical computing, cardiovascular hemodynamics and biomechanics, including medical image segmentation, patient specific model construction, blood flow simulations, vessel biomechanics, and finite element flow simulations.
             </div>
-          </div>
+          </div> -->
           <!-- Broader Uses -->
-          <div class="media">
+<!--           <div class="media">
             <a class="pull-left">
               <img class="media-object" src="https://simvascular.github.io/img/Img_About_Aorta.png" alt="...">
             </a>
@@ -85,12 +95,12 @@
               The SimVascular project is funded by the NSF SSI program under Program Officers Dan Katz (ACI) and Sumanta Acharya (CBET)
             </div>
                             
-          </div>
+          </div> -->
 
-          <h2 class="top-buffer">What's New?</h2>
+          <!-- <h2 class="top-buffer">What's New?</h2> -->
           
           <!-- DESCRIPTION -->
-          <div class="media">
+<!--           <div class="media">
             <a class="pull-left">
               <img class="media-object" src="https://simvascular.github.io/img/Img_About_Coro.png" alt="...">
             </a>
@@ -107,7 +117,7 @@
               <li>Backflow stabilization</li>
               </ul>
             </div>
-          </div>
+          </div> -->
 
         </div>
 	</div>
